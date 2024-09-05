922976cb8b2ae37bfc2e02f1d9379011d9b7e251
Willgarvey <willmgarvey@gmail.com>
2024-09-05 13:16:11 -0500

    Added Projects to Solution

---
+++ AgileTeamFour.BL.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AgileTeamFour.BL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- AgileTeamFour.UI/obj/Debug/net8.0/AgileTeamFour.UI.AssemblyInfo.cs
+++ AgileTeamFour.UI/obj/Debug/net8.0/AgileTeamFour.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgileTeamFour.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba59a3bcb322564c58657e333505226d663d5dcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgileTeamFour.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgileTeamFour.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
