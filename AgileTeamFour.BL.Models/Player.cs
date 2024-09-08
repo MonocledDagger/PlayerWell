d098151e21157d58d1187f52b9b2e6a6bea665db
Glen Lehrer <300085063@fvtc.edu>
2024-09-08 17:10:01 -0500

    Added Player, Review, and Comment to BL Models.  PLayerManager is currently empty.

---
+++ AgileTeamFour.BL.Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileTeamFour.BL.Models
+{
+    public class Player
+    {
+        //Primary key
+        public int PlayerID { get; set; }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //This string would be an image path
+        public string IconPic { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
---
+++ AgileTeamFour.BL.Models/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileTeamFour.BL.Models
+{
+    public class Review
+    {
+        //Primary Key
+        public int ReviewID { get; set; }
+
+        //Minimum of 1, maximum of 5
+        private int starsOutOf5 = 1;
+        public int StarsOutOf5 
+        { 
+            get { return starsOutOf5; } 
+            set { if (value < 1 || value > 5) 
+                    starsOutOf5 = 1;
+                else starsOutOf5 = value; } 
+        }
+
+        public int ReviewText { get; set; }
+
+        //Foreign Key references a PlayerID, the primary key in the player Table
+        public int AuthorID { get; set; }
+
+        //Foreign Key references a PlayerID, the primary key in the player Table
+        public int RecipientID { get; set; }
+        
+    }
+}
