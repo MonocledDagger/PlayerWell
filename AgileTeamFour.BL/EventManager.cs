ffae3545ca7487092f003c943ae9e8ea09f2d4ea
Ricardo Guzman Ortiz <300064747@fvtc.edu>
2024-09-13 23:05:29 -0500

    added EventDetailsVM

--- AgileTeamFour.BL/EventManager.cs
+++ AgileTeamFour.BL/EventManager.cs
@@ -267,6 +267,8 @@ namespace AgileTeamFour.BL
                 throw;
             }
 
+
+
         }
     }
 }
---
+++ AgileTeamFour.UI/ViewModels/EventDetailsVM.cs
@@ -0,0 +1,15 @@
+﻿
+
+using AgileTeamFour.BL.Models;
+
+namespace AgileTeamFour.UI.ViewModels
+{
+    public class EventDetailsVM
+    {
+        public Events Event { get; set; }
+        public Game Game { get; set; }
+        public List<PlayerEvent> PlayerEvents { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+
+}
