ec030efeb44366e1ae5b51c1aab26889f1b32e9c
James Dictus <500146933@fvtc.edu>
2024-09-24 23:00:15 -0500

    Big Event Updates

--- AgileTeamFour.UI/ViewModels/EventDetailsVM.cs
+++ AgileTeamFour.UI/ViewModels/EventDetailsVM.cs
@@ -6,6 +6,7 @@ namespace AgileTeamFour.UI.ViewModels
     {
         public Events Event { get; set; }
         public Game Game { get; set; }
+        public List<Game> Games { get; set; } = new List<Game>();
         public IEnumerable<PlayerEvent> PlayerEvents { get; set; }
         public List<Comment> Comments { get; set; }
     }
