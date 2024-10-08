0f04377a5d2465aaf0a9c9f0e78c3cf66fb1436f
Willgarvey <willmgarvey@gmail.com>
2024-10-08 12:36:23 -0500

    FIxed image views for Games and Events. Added API default data for games.

--- AgileTeamFour.DB/PostDeployment/DefaultData/tblEvents.sql
+++ AgileTeamFour.DB/PostDeployment/DefaultData/tblEvents.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN
 	INSERT INTO tblEvents (EventID, GameID, EventName, Server, MaxPlayers, Type, Platform, Description, DateTime, AuthorId)
 	VALUES
-	(1001, 66, 'WoW Expansion Party', 'US East', 32, 'Raid', 'PC', 'Raid for the first day of the new Wow Expansion', '2024-08-12 19:00:00', 2),
-	(1002, 59, 'Weekly Social Meetup', 'Central', 25, 'Social', 'All', 'Meetup for the Current Game Members', '2024-11-11 04:30:00', 3),
-	(1003, 45, 'Fallout: The Boardgame', 'Western', 4, 'Role Play', 'PC', 'A Fallout themed board game I need 3 more people to play with!', '2024-09-23 11:45:00', 4);
+	(1001, 23599, 'WoW Expansion Party', 'US East', 32, 'Raid', 'PC', 'Raid for the first day of the new Wow Expansion', '2024-08-12 19:00:00', 2),
+	(1002, 779, 'Weekly Social Meetup', 'Central', 25, 'Social', 'All', 'Meetup for the Current Game Members', '2024-11-11 04:30:00', 3),
+	(1003, 3070, 'Fallout: The Boardgame', 'Western', 4, 'Role Play', 'PC', 'A Fallout themed board game I need 3 more people to play with!', '2024-09-23 11:45:00', 4);
 END
--- AgileTeamFour.DB/PostDeployment/DefaultData/tblGames.sql
+++ AgileTeamFour.DB/PostDeployment/DefaultData/tblGames.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN
 	INSERT INTO tblGames (GameID, GameName, Platform, Description, Picture, Genre)
 	VALUES
-	(66, 'World of Warcraft', 'PC', 'An open world game with endless quests for power and might!', '.\wow.png','MMORPG'),
-	(59, 'Roblox', 'PC', 'A fun game to build and spend time with friends,','.\roblox.png','Social'),
-	(45, 'Fallout: The Boardgame', 'PC', 'A table top role playing game.', '.\falloutboardgame.png','Tabletop');
+	(23599, 'World of Warcraft', 'PC', 'An open world game with endless quests for power and might!', 'https://media.rawg.io/media/games/0d9/0d930ea604ee240c5af30c58f73ddf48.jpg','MMORPG'),
+	(779, 'Roblox', 'PC', 'A fun game to build and spend time with friends,','https://media.rawg.io/media/games/3af/3af386b6e26be6741b711ae6215ef42f.jpg','Social'),
+	(3070, 'Fallout 4', 'Xbox', 'A interactive shooter role playing game.', 'https://media.rawg.io/media/games/d82/d82990b9c67ba0d2d09d4e6fa88885a7.jpg','RPG');
 END
