67b8878e1973fe7e019d555fc00cbd6b8165bcdb
James Dictus <500146933@fvtc.edu>
2024-10-31 11:04:03 -0500

    Big Friend Update

--- AgileTeamFour.UI/ViewModels/FriendVM.cs
+++ AgileTeamFour.UI/ViewModels/FriendVM.cs
@@ -3,9 +3,14 @@
     public class FriendVM
     {
         public Friend Friend { get; set; }
-        public User User { get; set; }
 
-        public IEnumerable<FriendVM> Friends { get; set; }
-        public IEnumerable<FriendVM> PendingRequests { get; set; }
+
+        public User UserReceiver { get; set; }
+        public User UserSender { get; set; }
+
+        public List<User> Users { get; set; }
+
+        //public IEnumerable<FriendVM> Friends { get; set; }
+        //public IEnumerable<FriendVM> PendingRequests { get; set; }
     }
 }
