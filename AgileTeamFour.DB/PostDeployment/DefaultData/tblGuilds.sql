1a6131a7b27e6eb5cf08888c728230339315a431
James Dictus <500146933@fvtc.edu>
2024-11-08 20:37:29 -0600

    CSS changes, default data for Guild updated

--- AgileTeamFour.DB/PostDeployment/DefaultData/tblGuilds.sql
+++ AgileTeamFour.DB/PostDeployment/DefaultData/tblGuilds.sql
@@ -2,6 +2,7 @@
 	INSERT INTO tblGuild(GuildId, GuildName, Description, LeaderId)
 	VALUES
 	(1,'Hank Heroes','We are a guild consisting entirely of people named Hank',1),
-	(2,'Tom Boys','We are Toms Boys',2)
+	(2,'Tom Boys','We are Toms Boys',2),
+	(3,'The Pillars of Infinity','We Get the Cool Name',3)
 	
 END
