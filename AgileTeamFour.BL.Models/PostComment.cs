e5e2e864055b5266c853a3f98630701689535cba
guzmanor4747 <300064747@fvtc.edu>
2024-11-15 18:50:22 -0600

    added tblPostComments, manager and conrollers

---
+++ AgileTeamFour.BL.Models/PostComment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileTeamFour.BL.Models
+{
+    public class PostComment
+    {
+        public int CommentID { get; set; }
+        public int PostID { get; set; }
+        public int AuthorID { get; set; }
+        public DateTime TimePosted { get; set; }
+        public string Text { get; set; }
+        public int? ParentCommentID { get; set; }
+        public List<PostComment> Replies { get; set; } = new List<PostComment>();
+        public PostComment Comments { get; set; }
+       
+    }
+
+
+}
---
+++ AgileTeamFour.UI/Controllers/PostCommentController.cs
@@ -0,0 +1,71 @@
+﻿
+using AgileTeamFour.UI.Models;
+using AgileTeamFour.UI.ViewModels;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace AgileTeamFour.Web.Controllers
+{
+    public class PostCommentController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View(PostCommentManager.Load());
+
+        }
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            if (Authenticate.IsAuthenticated(HttpContext))
+            {
+                return View();
+            }
+            else
+            {
+                TempData["error"] = "Need to be logged in to Post";
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            }
+
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(PostComment post)
+        {
+
+            try
+            {
+                User user = GetLoggedInUser();
+                ViewBag.Title = "Create a User";
+
+                post.AuthorID = user.UserID;
+                post.TimePosted = DateTime.Now;
+                // Set image to an empty string if it is null
+                //post.Image = post.Image ?? "";
+                int result = PostCommentManager.Insert(post);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Title = "Create";
+                ViewBag.Error = ex.Message;
+                return View(post);
+            }
+        }
+
+        private User GetLoggedInUser()
+        {
+            return HttpContext.Session.GetObject<User>("user");
+        }
+    }
+}
+
+
+
+
+
+    
+
